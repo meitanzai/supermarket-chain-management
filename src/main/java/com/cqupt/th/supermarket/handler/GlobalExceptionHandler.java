c54280d1b4ebb1102eb2b61b13f19a9cd4cb7989
“Carytrue” <1607587113@qq.com>
2023-04-04 12:28:18 +0800

    init

--- src/main/java/com/cqupt/th/supermarket/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cqupt/th/supermarket/handler/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 /**
