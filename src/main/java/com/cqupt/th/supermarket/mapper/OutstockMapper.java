2a2295bd4b68ba13fa68e55d9554cb48765ee542
“Carytrue” <1607587113@qq.com>
2023-04-07 13:47:36 +0800

    init

--- src/main/java/com/cqupt/th/supermarket/mapper/OutstockMapper.java
+++ src/main/java/com/cqupt/th/supermarket/mapper/OutstockMapper.java
@@ -6,7 +6,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 /**
 * @author 16075
 * @description 针对表【outstock】的数据库操作Mapper
-* @createDate 2023-04-07 12:48:21
+* @createDate 2023-04-07 13:47:02
 * @Entity com.cqupt.th.supermarket.entity.Outstock
 */
 public interface OutstockMapper extends BaseMapper<Outstock> {
